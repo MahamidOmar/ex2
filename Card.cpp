06650ab7579eb42ec35559745e5a3d0da6a19914
MahamidOmar <omarm@campus.technion.ac.il>
2022-05-11 22:10:07 +0300

    v 0.14

--- Card.cpp
+++ Card.cpp
@@ -7,6 +7,7 @@ Card::Card(CardType type, const CardStats& stats)
 {
     this->m_effect = type;
     this->m_stats = stats;
+    /*
     if(m_stats.loot < 0)
         m_stats.loot = 0;
     if(m_stats.force < 0)
@@ -19,7 +20,7 @@ Card::Card(CardType type, const CardStats& stats)
         m_stats.buff = 0;
     if(m_stats.hpLossOnDefeat < 0)
         m_stats.hpLossOnDefeat = 0;
-
+    */
 }
 
 void Card::applyEncounter(Player& player) const {
